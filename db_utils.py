419e8f8976667a642cd3b073c8c3b2c729f58c1d
m0sthuge <hall.patrick42@gmail.com>
2023-11-21 13:52:45 -0600

    db_utils: reformatted, added a table creation tool for postgresql. data_utils: reformatted.

--- db_utils.py
+++ db_utils.py
@@ -7,11 +7,19 @@ import os
 import sqlite3 as sql
 import psycopg as pg
 import re
+from dotenv import load_dotenv
+import os
+from pymongo.mongo_client import MongoClient
+
+
+"""
+SQLITE TOOLS
+"""
 
 DB_NAME = 'default.sqlite'
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__),DB_NAME)
 
-#sets DEFAULT_PATH to desire path
+#sets DEFAULT_PATH to desired path. specifically used for sqlite.
 def set_DEFAULT_PATH(db_name = None,db_loc = None):
     global DEFAULT_PATH
     pattern = r'\.(sqlite|db)$'
@@ -47,6 +55,42 @@ def prepare_dict(list_of_dicts):
     list_of_tuples = [tuple(value for value in record.values()) for record in list_of_dicts] #syntax for this line is adapted from chatGPT
     return list_of_tuples
 
+"""
+POSTGRESQL TOOLS
+"""
+
+#Creates a a new table using column headers from a csv.
+def create_table_columns_from_csv(csv_file_path,schema,new_table_name,delim=';'):
+    load_dotenv()
+    dbname = os.environ.get('DB_NAME')
+    user = os.environ.get("USER")
+    password = os.environ.get("PASSWORD")
+
+    # Create a connection + cursor to the PostgreSQL database
+    cur,conn = db.initiate_pg_cursor(dbname,user,password) #connects to db and creates cursor object
+
+    # Path to your large CSV file
+    csv_file_path = '/Users/pathall/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/GEOG 5050 - Spatial Stats/Final_Project/Data/Staging/airbnb-listings.csv'
+
+
+    df = pd.read_csv(csv_file_path, sep=delim, nrows=1)  # Read only the first row to get column names
+    column_names = df.columns.tolist()
+    corrected_column_names = du.replace_spaces_in_list(column_names)
+
+    print(corrected_column_names)
+    print(len(corrected_column_names))
+
+    # Generate the SQL table creation statement
+    sql_statement = f"CREATE TABLE {schema}.{new_table_name} ({', '.join([f'{col} VARCHAR' for col in corrected_column_names])});"
+
+    # Execute the SQL statement to create the table
+    cur.execute(sql_statement)
+    conn.commit()
+    conn.close()
+
+    print(f'Table "{schema}.{table_name}" has been created in the database.')
+
+#Initiates a connection and cursor for local postgreSQL session
 def initiate_pg_cursor(dbname,user,password):
 
     #could be made more secure by making username and password an environmental variable. 
@@ -61,9 +105,9 @@ def initiate_pg_cursor(dbname,user,password):
 
     return cursor,connection
 
-from dotenv import load_dotenv
-import os
-from pymongo.mongo_client import MongoClient
+"""
+MONGODB TOOLS
+"""
 
 #adapted from MongoDB documentation.
 def get_client():
