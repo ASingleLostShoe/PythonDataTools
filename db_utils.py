a17db1008a7b04febdebc6e97b7e451d08d07892
m0sthuge <hall.patrick42@gmail.com>
2024-10-22 12:16:19 -0600

    fixed csv filepath error for create_table_columns_from_csv(). Added parameter notes for all postgreSQL functions.

--- db_utils.py
+++ db_utils.py
@@ -62,6 +62,21 @@ POSTGRESQL TOOLS
 
 #Creates a a new table using column headers from a csv.
 def create_table_columns_from_csv(csv_file_path,schema,new_table_name,delim=';'):
+
+    """
+    Creates a new table using column headers from a csv. Does not match data types, all columns will be
+    varchar, and will need to be copied ot a new table as their true data type.
+
+    Parameters:
+    csv_file_path (str): The path of the csv to create db table from.
+    schema (str): name of schema in db to write table to.
+    new_table_name (str): name of new table being created in schema.
+    delim (str): delimiting character in csv. defaults to ';'.
+
+    Returns:
+    nothing. Self-contained function.
+    """
+
     load_dotenv()
     dbname = os.environ.get('DB_NAME')
     user = os.environ.get("USER")
@@ -69,9 +84,6 @@ def create_table_columns_from_csv(csv_file_path,schema,new_table_name,delim=';')
 
     # Create a connection + cursor to the PostgreSQL database
     cur,conn = initiate_pg_cursor(dbname,user,password) #connects to db and creates cursor object
-
-    # Path to your large CSV file
-    csv_file_path = '/Users/pathall/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/GEOG 5050 - Spatial Stats/Final_Project/Data/Staging/airbnb-listings.csv'
 
 
     df = pd.read_csv(csv_file_path, sep=delim, nrows=1)  # Read only the first row to get column names
@@ -89,18 +101,33 @@ def create_table_columns_from_csv(csv_file_path,schema,new_table_name,delim=';')
     conn.commit()
     conn.close()
 
-    print(f'Table "{schema}.{table_name}" has been created in the database.')
+    print(f'Table "{schema}.{new_table_name}" has been created in the database.')
 
 #Initiates a connection and cursor for local postgreSQL session
 def initiate_pg_cursor(dbname,user,password):
 
-    #could be made more secure by making username and password an environmental variable. 
-    #Need more time to figure that one out.
+    """
+    Initiates connection and cursor objects for a local postgresql session. most secure way to use
+    this function is to pass user and password parameters as environmental variables (see python-dotenv).
+    Connects to default port (5432).
+
+    Parameters:
+    dbname (str): name of database
+    user (str): username. Best if passed from envionmental variable.
+    password (str): password. Best if passed from environmental variable.
+    
+    Returns:
+    cursor (pg cursor object): pass SQL queries to db and retrieve data from db.
+    connection (pg connection object): establish and manage connection to db.
+
+    """
+   
     try:
         connection = pg.connect(f"dbname={dbname} user={user} password={password}")
 
     except:
         print("error: connection not valid. Try changing connection parameter.")
+    
 
     cursor = connection.cursor()
 
